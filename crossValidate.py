ad67e66559491f5bdf5ee22afef7fdf29e63f65b
rfey <feyr@oregonstate.edu>
2020-03-11 18:21:14 +0000

    Added code to balance fold sizes in crossValidate.py

--- crossValidate.py
+++ crossValidate.py
@@ -14,23 +14,53 @@ if len(sys.argv) != 3 or '-h' in sys.argv or '--help' in sys.argv:
     sys.exit()
 
 # SUBROUTINES
-def splitData(n, groups, data):
-    # get group IDs
-    groupIDs = random.sample(list(groups.keys()), k=n)  # sample without replacement
-    # retrieve actual data corresponding to IDs
-    SNPlist = []
-    for ID in groupIDs:
-        SNPs = groups[ID]
-        SNPlist.extend(SNPs)
-    # avoid key error
-    check = data['name'].isin(SNPlist)
-    SNPfeatures = data[check.values]
-    # save labels
-    labels = SNPfeatures['label'].to_numpy()
-    SNPfeatures.drop(['name','label'],axis='columns',inplace=True)
-    SNPfeatures = SNPfeatures.to_numpy()
-    return SNPfeatures, labels
+def splitData(k, n, groups, data):
+    folds = [] # folds will be a list of k dataframes
+    # make a copy of the groups dict
+    groupsCopy = groups.copy()
+    for i in range(0,k):
+#        print(len(groupsCopy.keys()))  # to check that we start with all groups for each rep
+        # get group IDs
+        groupIDs = random.sample(list(groupsCopy.keys()), k=n)  # sample without replacement
+        # retrieve actual data corresponding to IDs
+        SNPlist = []
+        for ID in groupIDs:
+            SNPs = groupsCopy[ID]
+            SNPlist.extend(SNPs)
+        # avoid key error
+        check = data['name'].isin(SNPlist)
+        SNPfeatures = data[check.values]
+        # remove used groupIDs from groups dict
+        for ID in groupIDs:
+            if ID in groupsCopy:
+                del groupsCopy[ID]
+        folds.append(SNPfeatures)
+    return folds
 
+def balanceFoldSizes(folds):
+    foldList = []
+    minFold = min(len(featureDF) for featureDF in folds) # get fold with fewest SNPs
+#    print(str(minFold) + ' minimum fold') # check size of minimum fold size
+    # reduce number of all other folds
+    for featureDF in folds:
+        if len(featureDF) > minFold:
+            toDrop = len(featureDF) - minFold  # number of cSNPs to remove
+            # remove random cSNPs; 0 is the negative label; .index gets row indices as list-like object, req. input for .drop()
+            featureDF = featureDF.drop(featureDF.query('label == 0').sample(n=toDrop).index)
+        foldList.append(featureDF)
+    return foldList
+
+def sliceLabels(folds):
+    foldList, labelList = [],[]
+    # save labels
+    for SNPfeatures in foldList:
+        labels = SNPfeatures['label'].to_numpy()
+        SNPfeatures.drop(['name','label'],axis='columns',inplace=True)
+        SNPfeatures = SNPfeatures.to_numpy()
+        foldList.append(SNPfeatures)
+        labelList.append(labels)
+    return foldList, labelList
+    
 def runCV(folds,labels):
     for i in range(0,len(folds)):
         trainMatrix = folds[i]
@@ -75,9 +105,7 @@ nreps = 5
 n = round(len(groups)/k)  # number of groups per fold
 for i in range(0,nreps):
     print(i)
-    folds, labelList = [],[] # folds will be a list of k numpy arrays
-    for i in range(0,k):
-        SNPfeatures, labels = splitData(n,groups,dataDF) # split data into k groups
-        folds.append(SNPfeatures)
-        labelList.append(labels)
-    runCV(folds,labelList)  # run k-fold cross validation
+    folds = splitData(k,n,groups,dataDF) # split data into k groups
+    balancedFolds = balanceFoldSizes(folds) # make sure we have same number of SNPs per fold
+    foldList, labelList = sliceLabels(balancedFolds) # remove labels from feature data and save separately
+    runCV(foldList,labelList)  # run k-fold cross validation
