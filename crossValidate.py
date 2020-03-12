134e03de3e3ff2c417455038f982cf1f63af8181
rfey <feyr@oregonstate.edu>
2020-03-12 21:13:23 +0000

    functionalize crossValidate script

--- crossValidate.py
+++ crossValidate.py
@@ -1,17 +1,10 @@
-# rmf 3.9.2020, last modified 3.11.2020
+# rmf 3.9.2020, last modified 3.12.2020
 
 import sys, random
 import pandas
 import numpy as np
 from sklearn.model_selection import cross_validate
 import xgboost as xgb
-# import code to train/test somehow?
-
-# USAGE
-usage = 'python ' + sys.argv[0] + ' <data file> <groups file>'
-if len(sys.argv) != 3 or '-h' in sys.argv or '--help' in sys.argv:
-    print(usage)
-    sys.exit()
 
 # SUBROUTINES
 def splitData(k, n, groups, data):
@@ -76,36 +69,33 @@ def getRemainder(data,i):
     for x in remainder:
         remainderList.extend(x)
     return remainderList
-    
-# ARGUMENTS and MAIN
-dataFile = sys.argv[1]
-groupsFile = sys.argv[2]
 
-# read data file
-dataDF = pandas.read_csv(dataFile, sep='\t')
+def getKfolds(dataFile,groupsFile,k,nreps):
+    # read data file
+    dataDF = pandas.read_csv(dataFile, sep='\t')
 
-# read groups file
-groups,sizes = {},{}
-with open(groupsFile,'r') as groupsFile:
-    next(groupsFile) # skip header
-    for line in groupsFile:
-        chrom, start, stop, SNPname, groupID, groupSize = line.strip().split('\t')
-        # save SNP IDs for each group
-        if groupID not in groups:
-            groups[groupID] = []
-        groups[groupID].append(SNPname)
-        # save size of each group
-        if groupID not in sizes:
-            sizes[groupID] = groupSize
-groupsFile.close()
+    # read groups file
+    groups,sizes = {},{}
+    with open(groupsFile,'r') as groupsFile:
+        next(groupsFile) # skip header
+        for line in groupsFile:
+            chrom, start, stop, SNPname, groupID, groupSize = line.strip().split('\t')
+            # save SNP IDs for each group
+            if groupID not in groups:
+                groups[groupID] = []
+            groups[groupID].append(SNPname)
+            # save size of each group
+            if groupID not in sizes:
+                sizes[groupID] = groupSize
+    groupsFile.close()
 
-# split data
-k = 10
-nreps = 5
-n = round(len(groups)/k)  # number of groups per fold
-for i in range(0,nreps):
-    print(i)
-    folds = splitData(k,n,groups,dataDF) # split data into k groups
-    balancedFolds = balanceFoldSizes(folds) # make sure we have same number of SNPs per fold
-    foldList, labelList = sliceLabels(balancedFolds) # remove labels from feature data and save separately
-    runCV(foldList,labelList)  # run k-fold cross validation
+    # split data
+    returnFolds = []
+    n = round(len(groups)/k)  # number of groups per fold
+    for i in range(0,nreps):
+        print(i)
+        folds = splitData(k,n,groups,dataDF) # split data into k groups
+        balancedFolds = balanceFoldSizes(folds) # make sure we have same number of SNPs per fold
+        foldList, labelList = sliceLabels(balancedFolds) # remove labels from feature data and save separately
+        returnFolds.append((foldList,labelList))
+    return returnFolds
